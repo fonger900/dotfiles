9e9b8bcb095232476e01fbd64b0c1315b2db5f98
fonger900 <fonger900@gmail.com>
2025-05-07 16:46:09 +0700

    refactor

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "🧬 Setting up dotfiles..."
 ln -s $PWD/.zshrc ~/.zshrc
 ln -s $PWD/.tmux.conf ~/.tmux.conf
 # ln -s $PWD/.config/nvim ~/.config/nvim
-ln -s $PWD/.config/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
-ln -s $PWD/.config/starship.toml ~/.config/starship.toml
+ln -s $PWD/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
+ln -s $PWD/starship.toml ~/.config/starship.toml
 
 echo "✅ Done. Restart terminal to see changes."
