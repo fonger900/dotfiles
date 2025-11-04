9c7afbb6a67804377a34814ea2772df640cf085e
fonger900 <fonger900@gmail.com>
2025-11-04 11:52:40 +0700

    refactor: add noice.nvim plugin for enhanced command palette and notifications

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -62,7 +62,7 @@ return {
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { { "filename", path = 1 } },
+        lualine_c = { { "filename", path = 1 }, { "noice" } },
         lualine_x = { "encoding", "fileformat", "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
@@ -116,6 +116,31 @@ return {
         end,
       })
     end,
+  },
+
+  -- Noice for command palette and notifications
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = false,
+      },
+    },
   },
 
 }
