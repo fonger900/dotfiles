9e00ac717fce3f98422f93a16939860242aca997
fonger900 <fonger900@gmail.com>
2025-11-05 11:27:55 +0700

    feat: enhance LSP configuration with markdown rendering overrides and improved documentation display

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -186,10 +186,19 @@ return {
     event = "VeryLazy",                        -- Load after startup
     dependencies = { "MunifTanjim/nui.nvim" }, -- UI component library
     opts = {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
       presets = {
         bottom_search = true,         -- Use bottom search bar
         command_palette = true,       -- Command palette style cmdline
         long_message_to_split = true, -- Open long messages in split
+        lsp_doc_border = true,        -- add a border to hover docs and signature help
       },
     },
   },
