451b305d6217a4aa4f243ca44c5bac6acbd40136
fonger900 <fonger900@gmail.com>
2025-11-12 18:24:30 +0700

    fix Which-key.nvim not working

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -76,33 +76,27 @@ return {
       vim.o.timeout = true
       vim.o.timeoutlen = 300
     end,
-    opts = function()
-      return {
-        preset = "modern",
-        spec = {
-          { "<leader><tab>", group = "tabs" },
-          { "<leader>b",     group = "buffer" },
-          { "<leader>c",     group = "code" },
-          { "<leader>f",     group = "file/find" },
-          { "<leader>g",     group = "git" },
-          { "<leader>gh",    group = "hunks" },
-          { "<leader>q",     group = "quit/session" },
-          { "<leader>s",     group = "search" },
-          { "<leader>t",     group = "terminal" },
-          { "<leader>u",     group = "ui" },
-          { "<leader>w",     group = "windows" },
-          { "<leader>x",     group = "diagnostics/quickfix" },
-          { "[",             group = "prev" },
-          { "]",             group = "next" },
-          { "g",             group = "goto" },
-          { "gs",            group = "surround",            mode = { "n", "v" } },
-        },
-      }
-    end,
-    config = function(_, opts)
-      local wk = require("which-key")
-      wk.setup(opts)
-    end,
+    opts = {
+      preset = "modern",
+      spec = {
+        { "<leader><tab>", group = "tabs" },
+        { "<leader>b",     group = "buffer" },
+        { "<leader>c",     group = "code" },
+        { "<leader>f",     group = "file/find" },
+        { "<leader>g",     group = "git" },
+        { "<leader>gh",    group = "hunks" },
+        { "<leader>q",     group = "quit/session" },
+        { "<leader>s",     group = "search" },
+        { "<leader>t",     group = "terminal" },
+        { "<leader>u",     group = "ui" },
+        { "<leader>w",     group = "windows" },
+        { "<leader>x",     group = "diagnostics/quickfix" },
+        { "[",             group = "prev" },
+        { "]",             group = "next" },
+        { "g",             group = "goto" },
+        { "gs",            group = "surround",            mode = { "n", "v" } },
+      },
+    },
   },
 
   -- Better escape
