f81e7414eaa6f7538ff8e3ec58dec18c2efe41f6
fonger900 <fonger900@gmail.com>
2025-11-17 09:44:24 +0700

    refactor: streamline WezTerm configuration by consolidating font and color settings, removing unused options

--- wezterm.lua
+++ wezterm.lua
@@ -1,127 +1,32 @@
--- ==========================================
--- Wezterm Configuration for Fullstack Development
--- ==========================================
+-- Minimal, fast WezTerm configuration
 
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
--- Prefer the config_builder helper when available
 local config = wezterm.config_builder and wezterm.config_builder() or {}
 
 -- ==========================================
 -- Helper Functions
 -- ==========================================
 
-local function apply_font(config)
+local function apply_font_and_colors(config)
   config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Regular' })
   config.font_size = 12.5
   config.line_height = 1.2
-  config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
-end
-
-local function apply_colors(config)
   config.color_scheme = 'Catppuccin Mocha'
-  config.colors = {
-    tab_bar = {
-      background = '#1e1e2e',
-      active_tab = {
-        bg_color = '#313244',
-        fg_color = '#cdd6f4',
-        intensity = 'Bold',
-      },
-      inactive_tab = {
-        bg_color = '#1e1e2e',
-        fg_color = '#6c7086',
-      },
-      inactive_tab_hover = {
-        bg_color = '#181825',
-        fg_color = '#cdd6f4',
-      },
-      new_tab = {
-        bg_color = '#1e1e2e',
-        fg_color = '#6c7086',
-      },
-      new_tab_hover = {
-        bg_color = '#181825',
-        fg_color = '#cdd6f4',
-      },
-    },
-  }
 end
 
 local function apply_window(config)
   config.window_background_opacity = 0.95
-  config.macos_window_background_blur = 30
-
-  config.window_padding = {
-    left = 12,
-    right = 12,
-    top = 12,
-    bottom = 8,
-  }
-
   config.window_decorations = 'RESIZE'
   config.window_close_confirmation = 'NeverPrompt'
-
   config.initial_cols = 120
   config.initial_rows = 30
 end
 
 local function apply_tab_bar(config)
-  config.enable_tab_bar = true
-  config.use_fancy_tab_bar = true
   config.hide_tab_bar_if_only_one_tab = true
-  config.show_tab_index_in_tab_bar = true
-  config.tab_max_width = 32
-end
-
-local function setup_tab_title_formatter()
-  wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
-    local title = tab.tab_title
-    -- Use the current working directory basename if no title is set
-    if title and #title > 0 then
-      title = title
-    else
-      title = tab.active_pane.title
-    end
-
-    -- Extract just the directory name
-    local cwd = title:match("([^/]+)/?$") or title
-
-    -- Add icon based on process
-    local process_icons = {
-      ['docker'] = '🐳',
-      ['docker-compose'] = '🐳',
-      ['node'] = '⬢',
-      ['npm'] = '📦',
-      ['yarn'] = '📦',
-      ['pnpm'] = '📦',
-      ['python'] = '🐍',
-      ['python3'] = '🐍',
-      ['nvim'] = '',
-      ['vim'] = '',
-      ['git'] = '',
-      ['zsh'] = '',
-      ['bash'] = '',
-    }
-
-    local process_name = tab.active_pane.foreground_process_name
-    local icon = '  '
-    if process_name then
-      for proc, proc_icon in pairs(process_icons) do
-        if process_name:find(proc) then
-          icon = proc_icon .. ' '
-          break
-        end
-      end
-    end
-
-    local index = tab.tab_index + 1
-
-    return {
-      { Text = ' ' .. index .. ': ' .. icon .. cwd .. ' ' },
-    }
-  end)
+  config.use_fancy_tab_bar = false
 end
 
 local function apply_cursor_and_scrollback(config)
@@ -383,73 +288,24 @@ local function apply_mouse_bindings(config)
 end
 
 local function apply_launch_menu(config)
-  config.launch_menu = {
-    {
-      label = '🚀 New Full Stack Project',
-      args = { 'zsh', '-c', 'cd ~/Projects && pwd' },
-    },
-    {
-      label = '🐍 Python Virtual Environment',
-      args = { 'zsh', '-c', 'cd ~/Projects && source venv/bin/activate && zsh' },
-    },
-    {
-      label = '⬢ Node Development',
-      args = { 'zsh', '-c', 'cd ~/Projects && node --version && zsh' },
-    },
-    {
-      label = '🐳 Docker Dashboard',
-      args = { 'zsh', '-c', 'docker ps -a' },
-    },
-    {
-      label = '📝 Edit Configs',
-      args = { 'zsh', '-c', 'cd ~/dotfiles && nvim .' },
-    },
-  }
+  config.launch_menu = nil
 end
 
 local function apply_hyperlink_rules(config)
   config.hyperlink_rules = wezterm.default_hyperlink_rules()
-
-  table.insert(config.hyperlink_rules, {
-    regex = [[\blocalhost:\d+\b]],
-    format = 'http://$0',
-  })
-
-  table.insert(config.hyperlink_rules, {
-    regex = [[127\.0\.0\.1:\d+]],
-    format = 'http://$0',
-  })
-
-  table.insert(config.hyperlink_rules, {
-    regex = [[#\d+]],
-    format = 'https://github.com/$1',
-  })
 end
 
 local function apply_quick_select_patterns(config)
-  config.quick_select_patterns = {
-    -- File paths
-    '(?:[~./]|[a-zA-Z]:)[a-zA-Z0-9_\\-./]+',
-    -- Git commit hashes
-    '[a-f0-9]{7,40}',
-    -- UUIDs
-    '[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}',
-    -- IP addresses
-    '\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}',
-    -- Docker container IDs
-    '[a-f0-9]{12,64}',
-  }
+  config.quick_select_patterns = nil
 end
 
 -- ==========================================
 -- Apply Configuration Sections
 -- ==========================================
 
-apply_font(config)
-apply_colors(config)
+apply_font_and_colors(config)
 apply_window(config)
 apply_tab_bar(config)
-setup_tab_title_formatter()
 apply_cursor_and_scrollback(config)
 apply_performance(config)
 apply_keybindings(config)
