135b0b9182ef2b9d0b7431a707ec60be2bf5aaa3
fonger900 <fonger900@gmail.com>
2025-12-18 23:24:30 +0700

    add helper scripts

---
+++ scripts/debug_profile.zsh
@@ -0,0 +1,35 @@
+zmodload zsh/datetime
+
+echo "Starting profile..."
+
+t1=$EPOCHREALTIME
+source ~/dotfiles/zsh/path.zsh
+t2=$EPOCHREALTIME
+printf "Path: %.3fs\n" $(( t2 - t1 ))
+
+export ZSH="$HOME/.oh-my-zsh"
+zstyle ':omz:update' mode disabled
+plugins=(git docker)
+source "$ZSH/oh-my-zsh.sh"
+t3=$EPOCHREALTIME
+printf "OMZ: %.3fs\n" $(( t3 - t2 ))
+
+source ~/dotfiles/zsh/exports.zsh
+t4=$EPOCHREALTIME
+printf "Exports: %.3fs\n" $(( t4 - t3 ))
+
+source ~/dotfiles/zsh/aliases.zsh
+t5=$EPOCHREALTIME
+printf "Aliases: %.3fs\n" $(( t5 - t4 ))
+
+if command -v starship &> /dev/null; then
+  eval "$(starship init zsh)"
+fi
+t6=$EPOCHREALTIME
+printf "Starship: %.3fs\n" $(( t6 - t5 ))
+
+if command -v fastfetch &> /dev/null; then
+  fastfetch > /dev/null
+fi
+t7=$EPOCHREALTIME
+printf "Fastfetch: %.3fs\n" $(( t7 - t6 ))
---
+++ scripts/profile_zsh.zsh
@@ -0,0 +1,8 @@
+# Enable profiling
+zmodload zsh/zprof
+
+# Source the original config
+source ~/.zshrc
+
+# Run zprof
+zprof
